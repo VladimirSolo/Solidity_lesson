d6b25660a65e7a37616c25496e6d98609f0f0401
Vladimir Solo <freeman.wushu@mail.ru>
2025-02-23 20:21:23 +0300

    feat: add abstract contract ERC721URIStorage

---
+++ lesson-16-ERC721/ERC721URIStorage.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts
+
+pragma solidity ^0.8.20;
+
+import "./ERC721.sol";
+
+abstract contract ERC721URIStorage is ERC721 {
+    mapping(uint tokenId => string) private _tokenURIs;
+
+    function tokenURI(
+        uint tokenId
+    )
+        public
+        view
+        virtual
+        override
+        _requireMinted(tokenId)
+        returns (string memory)
+    {
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+
+        return super.tokenURI(tokenId);
+    }
+
+    function _setTokenURI(
+        uint256 tokenId,
+        string memory _tokenURI
+    ) internal virtual _requireMinted(tokenId) {
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function _burn(uint tokenId) internal virtual override {
+        super._burn(tokenId);
+
+        if (bytes(_tokenURIs[tokenId]).length != 0) {
+            delete _tokenURIs[tokenId];
+        }
+    }
+}
