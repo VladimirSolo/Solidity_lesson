502a4d641ed2d13e12b2da4fbed0a8801808ff00
Vladimir Solo <freeman.wushu@mail.ru>
2025-02-23 20:22:19 +0300

    feat: add abstract contract ERC721Enumerable

---
+++ lesson-16-ERC721/ERC721Enumerable.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+import "./ERC721.sol";
+import "./IERC721Enumerable.sol";
+
+abstract contract ERC721Enumerable is ERC721, IERC721Enumerable {
+    uint[] private _allTokens;
+
+    mapping(address owner => mapping(uint index => uint)) private _ownedTokens;
+    mapping(uint tokenId => uint) private _allTokensIndex;
+    mapping(uint tokenId => uint) private _ownedTokensIndex;
+
+    function totalSupply() public view returns (uint) {
+        return _allTokens.length;
+    }
+
+    function tokenByIndex(uint index) public view returns (uint) {
+        require(index < totalSupply(), "Out of bonds!");
+
+        return _allTokens[index];
+    }
+
+    function tokenOfOwnerByIndex(
+        address owner,
+        uint index
+    ) public view returns (uint) {
+        require(index < balanceOf(owner), "Out of bonds!");
+
+        return _ownedTokens[owner][index];
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC721) returns (bool) {
+        return (interfaceId == type(IERC721Enumerable).interfaceId ||
+            super.supportsInterface(interfaceId));
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint tokenId
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, tokenId);
+
+        if (from == address(0)) {
+            _addTokenToAllTokensEnumeration(tokenId);
+        } else if (from != to) {
+            _removeTokenFromAllTokensEnumeration(tokenId);
+        }
+
+        if (to == address(0)) {
+            _removeTokenFromAllTokensEnumeration(tokenId);
+        } else if (to != from) {
+            _addTokenToAllTokensEnumeration(tokenId);
+        }
+    }
+
+    function _addTokenToAllTokensEnumeration(uint tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
+        _allTokens.push(tokenId);
+    }
+
+    function _removeTokenFromAllTokensEnumeration(uint tokenId) private {
+        // To prevent a gap in the tokens array, we store the last token in the index of the token to delete, and
+        // then delete the last slot (swap and pop).
+
+        uint lastTokenIndex = _allTokens.length - 1;
+        uint tokenIndex = _allTokensIndex[tokenId];
+
+        // When the token to delete is the last token, the swap operation is unnecessary. However, since this occurs so
+        // rarely (when the last minted token is burnt) that we still do the swap here to avoid the gas cost of adding
+        // an 'if' statement (like in _removeTokenFromOwnerEnumeration)
+        uint lastTokenId = _allTokens[lastTokenIndex];
+
+        _allTokens[tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
+        _allTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
+
+        // This also deletes the contents at the last position of the array
+        delete _allTokensIndex[tokenId];
+        _allTokens.pop();
+    }
+
+    function _addTokenToOwnerEnumeration(address to, uint tokenId) private {
+        uint length = balanceOf(to) - 1;
+        _ownedTokens[to][length] = tokenId;
+        _ownedTokensIndex[tokenId] = length;
+    }
+
+    function _removeTokenFromOwnerEnumeration(
+        address from,
+        uint tokenId
+    ) private {
+        // To prevent a gap in from's tokens array, we store the last token in the index of the token to delete, and
+        // then delete the last slot (swap and pop).
+
+        uint lastTokenIndex = balanceOf(from);
+        uint tokenIndex = _ownedTokensIndex[tokenId];
+
+        mapping(uint index => uint) storage _ownedTokensByOwner = _ownedTokens[
+            from
+        ];
+
+        // When the token to delete is the last token, the swap operation is unnecessary
+        if (tokenIndex != lastTokenIndex) {
+            uint lastTokenId = _ownedTokensByOwner[lastTokenIndex];
+
+            _ownedTokensByOwner[tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
+            _ownedTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
+        }
+
+        // This also deletes the contents at the last position of the array
+        delete _ownedTokensIndex[tokenId];
+        delete _ownedTokensByOwner[lastTokenIndex];
+    }
+}
