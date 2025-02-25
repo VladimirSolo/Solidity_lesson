5f5abf6d2dd24a143315587f216d9197a03af37e
Vladimir Solo <freeman.wushu@mail.ru>
2025-02-25 20:09:16 +0300

    feat: add token cotract

---
+++ lesson-16-ERC721/Token.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "./ERC721.sol";
+import "./ERC721URIStorage.sol";
+import "./ERC721Enumerable.sol";
+
+contract Token is ERC721, ERC721URIStorage, ERC721Enumerable {
+    address public owner;
+    uint currentTokenId;
+
+    constructor() ERC721("Token", "TK") {
+        owner = msg.sender;
+    }
+
+    function safeMint(address to, string calldata tokenId) public {
+        require(owner == msg.sender, "Not an owner!");
+
+        _safeMint(to, tokenId);
+        _setTokenURI(currentTokenId, tokenId);
+        currentTokenId++;
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://";
+    }
+
+    function _burn(uint tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    )
+        public
+        view
+        virtual
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+}
