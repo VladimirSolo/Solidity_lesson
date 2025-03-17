731b9a1d68c0b496e44106cd3f0309cc3bf086ce
Vladimir Solo <freeman.wushu@mail.ru>
2025-03-17 23:56:32 +0200

    feat: som explain contract privacy

--- lesson-17-ethernaut/level_12/Privacy.sol
+++ lesson-17-ethernaut/level_12/Privacy.sol
@@ -34,3 +34,34 @@ Nothing in the Ethereum blockchain is private. The keyword private is merely an 
  Tools such as Foundry or Ethers can be used to read anything from storage. It can be tricky to read what you want though,
  since several optimization rules and techniques are used to compact the storage as much as possible.
  */
+
+/* 
+bool public locked: This occupies slot 0. Each state variable takes up a single slot 
+unless it is part of a struct or an array. bool takes 1 byte, but an entire 32-byte slot is allocated for it.
+
+uint256 public ID: This occupies slot 1. uint256 requires 32 bytes (1 full slot).
+
+uint8 private flattening: This will start in slot 2. uint8 only takes 1 byte.
+
+uint8 private denomination: Since uint8 also takes only 1 byte, it is packed into slot 2 along with flattening.
+
+uint16 private awkwardness: uint16 takes 2 bytes. Solidity packs this into slot 2 along with the other uint8 values because they collectively fit within 32 bytes.
+
+Slot 2 looks like this (in bytes): flattening (1) + denomination (1) + awkwardness (2) + 28 bytes padding = 32 bytes
+
+bytes32[3] private data: Arrays and structs are always stored in separate slots.
+ The array data has 3 elements of type bytes32, and each bytes32 takes 32 bytes (1 full slot).
+
+data[0] is stored in slot 3.
+
+data[1] is stored in slot 4.
+
+data[2] is stored in slot 5.
+
+Therefore, data[2] is in slot 5 because it is the third element of the bytes32[3] array,
+and each element of this array occupies its own 32-byte storage slot. 
+The preceding variables occupy slots 0 through 2, with the array data starting at slot 3 and extending 
+to slots 4 and 5 for its elements.
+
+Once found, cast it as bytes16 to truncate the packed zeros.
+ */
