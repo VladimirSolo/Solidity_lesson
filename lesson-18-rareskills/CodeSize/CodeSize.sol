6f601de5aeaadf6391b30777484ffefa103116c6
Vladimir Solo <freeman.wushu@mail.ru>
2025-05-21 21:00:09 +0300

    description add

--- lesson-18-rareskills/CodeSize/CodeSize.sol
+++ lesson-18-rareskills/CodeSize/CodeSize.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.13;
 
 contract CodeSize {
+    /**
+     * The challenge is to create a contract whose runtime code (bytecode) size is greater than 1kb but less than 4kb
+     */
     uint256 public value;
     mapping(address => uint256) public balances;
 
