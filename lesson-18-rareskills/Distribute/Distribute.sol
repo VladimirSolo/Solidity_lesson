48204dcefe6afe859d908e04bf646571c100f04b
Vladimir Solo <freeman.wushu@mail.ru>
2025-05-26 22:16:17 +0300

    feat: add solution for contract Distribute and som explain

--- lesson-18-rareskills/Distribute/Distribute.sol
+++ lesson-18-rareskills/Distribute/Distribute.sol
@@ -3,15 +3,36 @@ pragma solidity ^0.8.13;
 
 contract Distribute {
     /*
-        This exercise assumes you know how to sending Ether.
-        1. This contract has some ether in it, distribute it equally among the
-           array of addresses that is passed as argument.
-        2. Write your code in the `distributeEther` function.
-    */
+     This exercise assumes you know how to sending Ether.
+     1. This contract has some ether in it, distribute it equally among the
+     array of addresses that is passed as argument.
+     2. Write your code in the `distributeEther` function.
+     */
 
     constructor() payable {}
 
     function distributeEther(address[] memory addresses) public {
-        // your code here
+        require(addresses.length > 0, "No addresses provided");
+
+        uint256 totalBalance = address(this).balance;
+        require(totalBalance > 0, "No ether to distribute");
+
+        uint256 amountPerAddress = totalBalance / addresses.length;
+
+        for (uint256 i = 0; i < addresses.length; i++) {
+            require(addresses[i] != address(0), "Invalid address");
+
+            (bool success, ) = addresses[i].call{value: amountPerAddress}("");
+            require(success, "Transfer failed");
+        }
     }
 }
+
+/* 
+// Старый способ - может не работать:
+payable(recipient).transfer(amount);
+
+// Современный способ - надежнее:
+(bool success, ) = recipient.call{value: amount}("");
+require(success, "Transfer failed");
+ */
