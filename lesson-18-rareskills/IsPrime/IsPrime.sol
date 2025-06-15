561ad60ee88461b68da64713e264874bde40457e
Vladimir Solo <freeman.wushu@mail.ru>
2025-06-15 20:30:56 +0300

    feat: add solution contract IsPrime

--- lesson-18-rareskills/IsPrime/IsPrime.sol
+++ lesson-18-rareskills/IsPrime/IsPrime.sol
@@ -5,7 +5,15 @@ contract IsPrime {
     /**
      * The goal of this exercise is to return if "number" is prime or not (true or false)
      */
-    function isPrime(uint256 number) public view returns (bool) {
-        // your code here
+    function isPrime(uint256 number) public pure returns (bool) {
+        if (number < 2) return false;
+        if (number == 2 || number == 3) return true;
+        if (number % 2 == 0 || number % 3 == 0) return false;
+
+        for (uint256 i = 5; i * i <= number; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) return false;
+        }
+
+        return true;
     }
 }
