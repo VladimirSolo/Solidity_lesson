111080e5e4ba4abe806811ffe611ffa40b7d5d1b
Vladimir Solo <freeman.wushu@mail.ru>
2025-06-29 20:57:33 +0300

    feat: add solution contract OneWeekLockup

--- lesson-18-rareskills/OneWeekLockup/OneWeekLockup.sol
+++ lesson-18-rareskills/OneWeekLockup/OneWeekLockup.sol
@@ -2,26 +2,40 @@
 pragma solidity ^0.8.13;
 
 contract OneWeekLockup {
-    /**
-     * In this exercise you are expected to create functions that let users deposit ether
-     * Users can also withdraw their ether (not more than their deposit) but should only be able to do a week after their last deposit
-     * Consider edge cases by which users might utilize to deposit ether
-     *
-     * Required function
-     * - depositEther()
-     * - withdrawEther(uint256 )
-     * - balanceOf(address )
-     */
+    mapping(address => uint256) private balances;
+    mapping(address => uint256) private lastDepositTime;
 
-    function balanceOf(address user) public view returns (uint256) {
-        // return the user's balance in the contract
-    }
+    uint256 constant ONE_WEEK = 7 days;
 
+    /// @notice Deposit ether into the contract
     function depositEther() external payable {
-        /// add code here
+        require(msg.value > 0, "Must deposit more than 0");
+
+        balances[msg.sender] += msg.value;
+        lastDepositTime[msg.sender] = block.timestamp;
     }
 
+    /// @notice Withdraw ether from the contract (after one week from last deposit)
+    /// @param amount The amount to withdraw
     function withdrawEther(uint256 amount) external {
-        /// add code here
+        require(amount > 0, "Cannot withdraw zero");
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        require(
+            block.timestamp >= lastDepositTime[msg.sender] + ONE_WEEK,
+            "Must wait 1 week after last deposit"
+        );
+
+        balances[msg.sender] -= amount;
+
+        // Transfer ETH to sender
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "ETH transfer failed");
+    }
+
+    /// @notice View the balance of a user
+    /// @param user The address to query
+    /// @return The amount of ether deposited by the user
+    function balanceOf(address user) public view returns (uint256) {
+        return balances[user];
     }
 }
